331145deaa9d085bc3aa1856224961caafb5df87
Portno <j.remoundos@dotbydot.gr>
2019-04-22 14:28:14 +0300

    first commit

---
+++ src/Panacea.Modularity.RfidReader/IRfidReaderPlugin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panacea.Modularity.RfidReader
+{
+    public interface IRfidReaderPlugin : IPlugin
+    {
+        event EventHandler<string> CardConnected;
+        event EventHandler<string> CardDisconnected;
+        void SimulateCardTap(string s);
+    }
+}
